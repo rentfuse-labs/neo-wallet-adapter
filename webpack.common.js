7800e04e0309e7828d92bad1c8d999cfefa435cd
mfbz <michael.fabozzi@icloud.com>
2022-08-15 22:54:56 +0200

    Added cdn support

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+let env = process.env.NODE_ENV || 'development';
+
+module.exports = function (rootDir) {
+	return {
+		mode: env,
+		devtool: env === 'development' ? 'inline-source-map' : 'source-map',
+		entry: path.resolve(rootDir, 'src', 'index.ts'),
+		resolve: {
+			// Add `.ts` and `.tsx` as a resolvable extension.
+			extensions: ['.ts', '.tsx', '.js'],
+			fallback: {
+				crypto: false,
+				bufferutil: false,
+				'utf-8-validate': false,
+				zlib: false,
+				stream: false,
+				http: false,
+				https: false,
+				path: false,
+				net: false,
+				os: false,
+				tls: false,
+				fs: false,
+			},
+		},
+		module: {
+			rules: [
+				// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					options: {
+						projectReferences: true,
+						compilerOptions: {
+							sourceMap: env === 'development',
+							module: 'commonjs',
+						},
+					},
+				},
+			],
+		},
+		// plugins: [new CleanWebpackPlugin({verbose: true})]
+	};
+};
